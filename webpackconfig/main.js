88288d84d9a8d769f5e9d7b3a12c7087722b811b
kejiashan <297206795@qq.com>
2020-06-14 10:48:53 +0800

    监视html改动，自动产出build.js

---
+++ webpackconfig/main.js
@@ -0,0 +1,22 @@
+//es6 Moudule语法
+//整个项目的入口文件
+//从这个路径里面导入app对象,名字可以任意取   路径：相对路径
+// import App from "./App";
+import Vue from "../js/vue/vue.js";
+//拿到抛出的key
+// import {num1,num2,add} from "./App";
+//
+// console.log(num1);
+// console.log(num2);
+// console.log(add(3,5));
+
+//整个App.js模块加载
+import * as obj from './App';
+
+new Vue({
+    el:'#app',
+    components:{
+       App:obj.default
+    },
+    template:'<App/>'
+});
